41c597609428b9ebb047f1747a230344553800e9
Alex Larcheveque <larchevequealex@gmail.com>
2019-11-07 11:16:19 -0800

    Added More Context to see how multiple values would get passed down.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,8 +1,28 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
+  langChange(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
+
+  colorChange(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => this.langChange(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
-    return <button className="ui button primary"> Submit </button>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.colorChange(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
